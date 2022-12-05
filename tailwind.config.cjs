d913d9dbb31a514df13ae85996a0c356ec0254e4
Keith Robinson <kthrob@gmail.com>
2022-12-05 08:26:35 -0700

    First round of template customizations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,44 +1,29 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
-const colors = require('tailwindcss/colors');
 
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			colors: {
-				primary: colors.blue,
-				secondary: colors.pink,
+				primary: 'rgb(var(--color-primary) / <alpha-value>)',
+				secondary: 'var(--color-secondary)',
+			},
+			textColor: {
+				default: 'rgb(var(--color-text))',
+				offset: 'var(--color-text-offset)',
+			},
+			backgroundColor: {
+				default: 'rgb(var(--color-background) / <alpha-value>)',
+				offset: 'rgb(var(--color-background-offset) / <alpha-value>)',
+			},
+			borderColor: {
+				default: 'var(--color-border)',
 			},
 			fontFamily: {
 				sans: ["'InterVariable'", ...defaultTheme.fontFamily.sans],
 			},
 		},
 	},
-	plugins: [require('@tailwindcss/typography')],
-	darkMode: 'class',
+	plugins: [require('@tailwindcss/typography'), require('tailwindcss-fluid-type')],
+	darkMode: 'media',
 };
-
-/* 
-
-  Alternative tailwind.config.js
-  
-  NOTE: Add this fonts to <head>
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;700&display=swap" rel="stylesheet" />
-*/
-
-// module.exports = {
-//   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
-//   theme: {
-//     extend: {
-//       colors: {
-//         primary: colors.cyan,
-//         secondary: colors.lime,
-//       },
-//       fontFamily: {
-//         sans: ["'Nunito'", ...defaultTheme.fontFamily.sans],
-//       },
-//     },
-//   },
-//   plugins: [require("@tailwindcss/typography")],
-//   darkMode: "class",
-// };
