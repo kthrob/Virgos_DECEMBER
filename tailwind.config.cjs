a2b79062496dab8a8c2b33b86ec3035b71d87b7a
Keith Robinson <kthrob@gmail.com>
2022-12-06 10:12:50 -0700

    Add logo file, set colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,26 +1,61 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
+function convertToRGB(hexWithHash) {
+	var hexNoHash = hexWithHash.replace(/#(\S)/g, '$1');
+	var rbgArray = hexNoHash.match(/.{1,2}/g);
+	var rgbNoCommas = [parseInt(rbgArray[0], 16), parseInt(rbgArray[1], 16), parseInt(rbgArray[2], 16)];
+	return `${rgbNoCommas[0]},${rgbNoCommas[1]},${rgbNoCommas[2]}`;
+}
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			colors: {
-				primary: 'rgb(var(--color-primary) / <alpha-value>)',
-				secondary: 'var(--color-secondary)',
+				primary: 'var(--primary)',
+				secondary: 'var(--secondary)',
+				accent: 'var(--accent)',
 			},
 			textColor: {
-				default: 'rgb(var(--color-text))',
-				offset: 'var(--color-text-offset)',
+				default: 'var(--text)',
+				offset: 'var(--text-offset)',
 			},
 			backgroundColor: {
-				default: 'rgb(var(--color-background) / <alpha-value>)',
-				offset: 'rgb(var(--color-background-offset) / <alpha-value>)',
+				default: 'var(--background)',
+				primary: 'var(--background-primary)',
+				'default-glass': 'var(--background-transparent)',
+				offset: 'var(--background-offset)',
+				'offset-glass': 'var(--background-offset-transparent)',
 			},
 			borderColor: {
-				default: 'var(--color-border)',
+				default: 'var(--border)',
 			},
 			fontFamily: {
 				sans: ["'InterVariable'", ...defaultTheme.fontFamily.sans],
+			},
+		},
+		pallet: {
+			light: {
+				accent: '#0900A8',
+				primary: '#FFFFFF',
+				secondary: '#000000',
+				text: '#1D1D1F',
+				textOffset: '#86868b',
+				background: '#F5F5F7',
+				backgroundOffset: '#FAFAFA',
+				backgroundInvert: '#111111',
+				border: '#111827',
+			},
+			dark: {
+				accent: '#1A10B1',
+				primary: '#000000',
+				secondary: '#FFFFFF',
+				text: '#FFFFFF',
+				textOffset: '#86868B',
+				background: '#161617',
+				backgroundOffset: '#111111',
+				backgroundInvert: '#FAFAFA',
+				border: '#f1f5f9',
 			},
 		},
 	},
