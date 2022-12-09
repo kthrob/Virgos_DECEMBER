aa8183258a0b7dccab4f7339f9396e9ac27d7ac5
Keith Robinson <kthrob@gmail.com>
2022-12-08 18:42:04 -0700

    WIP refactoring slide cards for web-dev section

--- src/config.mjs
+++ src/config.mjs
@@ -1,15 +1,15 @@
 export const SITE = {
-	name: 'AstroWind',
+	name: 'Virgos Design',
 
-	origin: 'https://astrowind.vercel.app',
+	origin: 'https://virgosdesign.com',
 	basePathname: '/',
 	trailingSlash: false,
 
-	title: 'AstroWind — Your website with Astro + Tailwind CSS',
-	description: '🚀 AstroWind is a free and ready to start template to make your website using Astro and Tailwind CSS.',
+	title: 'Virgos Design | Infinite Possibilities',
+	description: 'Virgos Design is proudly serves small businesses in Queen Creek, AZ and nationwide.',
 
 	googleAnalyticsId: false, // or "G-XXXXXXXXXX",
-	googleSiteVerificationId: 'orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M',
+	googleSiteVerificationId: '',
 };
 
 export const BLOG = {
